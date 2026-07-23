313a3ce47fdaaca1bb2aae47a4228d2929d96e4a
tarek-amin01 <223136152+tarek-amin01@users.noreply.github.com>
2026-07-23 17:24:48 +0600

    setting up the database context

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using CafeSystem.Models;
+
+namespace CafeSystem.Data;
+
+public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<OrderItem> OrderItems { get; set; }
+    public DbSet<Cart> Carts { get; set; }
+    public DbSet<CartItem> CartItems { get; set; }
+    public DbSet<Inventory> Inventories { get; set; }
+    public DbSet<Staff> Staffs { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        
+        // Additional configuration if needed
+    }
+}
