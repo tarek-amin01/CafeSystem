332bb826c9034ffedb0056f2f9f48b1aa7b2c8c3
Jakiyasultanamira608-droid <318003530+jakiyasultanamira608-droid@users.noreply.github.com>
2026-08-14 22:16:42 +0600

    adding profile data fields

---
+++ Migrations/20251222143303_AddProfileFields.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CafeSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProfileFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ProfileImagePath",
+                table: "AspNetUsers",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProfileImagePath",
+                table: "AspNetUsers");
+        }
+    }
+}
